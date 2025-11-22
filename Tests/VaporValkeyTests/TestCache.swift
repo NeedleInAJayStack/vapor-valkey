1ef52ca7836c89ae6a9c87920ac88329d52c5429
Jay Herron <NeedleInAJayStack@protonmail.com>
2025-11-22 12:15:52 -0700

    feat: Adds Valkey Cache conformance

---
+++ Tests/VaporValkeyTests/TestCache.swift
@@ -0,0 +1,76 @@
+import Testing
+import Valkey
+import Vapor
+import VaporTesting
+import VaporValkey
+
+@Suite
+struct TestCache {
+    @Test func valkeyCacheGet() async throws {
+        try await withApp { app in
+            let client = ValkeyClient(
+                .hostname(valkeyHostname(), port: valkeyPort()),
+                eventLoopGroup: app.eventLoopGroup,
+                logger: app.logger
+            )
+            async let _ = client.run()
+
+            app.caches.use(.valkey(client))
+
+            let key = "\(Int.random())"
+            let value = "\(Int.random())"
+            // Manually populate Valkey as the JSON encoded representation, with quotes
+            try await client.set(.init(key), value: "\"\(value)\"")
+            try await #expect(app.cache.get(key, as: String.self) == value)
+
+            try await client.del(keys: [.init(key)])
+        }
+    }
+
+    // Test that cache get reflects what was manually put in Valkey
+    @Test func valkeyCacheSet() async throws {
+        try await withApp { app in
+            let client = ValkeyClient(
+                .hostname(valkeyHostname(), port: valkeyPort()),
+                eventLoopGroup: app.eventLoopGroup,
+                logger: app.logger
+            )
+            async let _ = client.run()
+
+            app.caches.use(.valkey(client))
+
+            let key = "\(Int.random())"
+            let value = "\(Int.random())"
+            try await app.cache.set(key, to: value)
+            // Manually check Valkey as the JSON encoded representation, with quotes
+            var buffer = try #require(await client.get(.init(key)))
+            #expect(buffer.readString(length: buffer.readableBytes) == "\"\(value)\"")
+
+            // Check that setting to nil deletes the cached key
+            let nilValue: String? = nil
+            try await app.cache.set(key, to: nilValue)
+            try await #expect(client.get(.init(key)) == nil)
+        }
+    }
+
+    // Test that cache get reflects what was manually put in Valkey
+    @Test func valkeyCacheDelete() async throws {
+        try await withApp { app in
+            let client = ValkeyClient(
+                .hostname(valkeyHostname(), port: valkeyPort()),
+                eventLoopGroup: app.eventLoopGroup,
+                logger: app.logger
+            )
+            async let _ = client.run()
+
+            app.caches.use(.valkey(client))
+
+            let key = "\(Int.random())"
+            let value = "\(Int.random())"
+            try await app.cache.set(key, to: value)
+            try await #expect(app.cache.get(key, as: String.self) == value)
+            try await app.cache.delete(key)
+            try await #expect(client.get(.init(key)) == nil)
+        }
+    }
+}
